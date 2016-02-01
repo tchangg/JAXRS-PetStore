0124e71043c9e892020b7cd3533c6c558b9d6ca8
Timothy Chang <timothywchang@yahoo.com>
2016-02-01 13:39:03 -0800

    Removing debugging code

--- src/main/java/com/petstore/PetStoreTest.java
+++ src/main/java/com/petstore/PetStoreTest.java
@@ -27,27 +27,19 @@ public class PetStoreTest extends JerseyTest {
 
 	@Test
 	public void testGetAllPetsByName() throws URISyntaxException {
-		WebResource webResource = client().resource("http://localhost:8080/");
-		//JSONArray json = new JSONArray(webResource.path("/JAXRS-PetStore/rest/PetStore/getAllPets").queryParam("sortBy", "name").get(String.class));
-		//JsonArray json = new JsonParser().parse(webResource.path("/JAXRS-PetStore/rest/PetStore/getAllPets").queryParam("sortBy", "name").get(String.class)).getAsJsonArray();
 		String petsResponse = HttpRequest.get("http://localhost:8080/JAXRS-PetStore/rest/PetStore/getAllPets", true, "sortBy", "name").body();
 		JsonArray json = new JsonParser().parse(petsResponse).getAsJsonArray();
-		System.out.println("json size: "+json.size());
 		assertEquals(numberOfPets(), json.size());
 		String name1 = "", name2 = "";
 		JsonElement jsonEle1, jsonEle2;
 		if(json.size() > 1) {
 			for(int i = 1; i < json.size(); i++) {
-				//assertTrue(json.getJSONObject(i-1)	.getString("name").compareToIgnoreCase(json.getJSONObject(i).getString("name")) <= 0);
 				jsonEle1 = json.get(i-1).getAsJsonObject().get("name");
 				if(jsonEle1 == null) {
 					name1 = "";
 				}
 				else {
 					name1 = jsonEle1.getAsString();
-					if(name1 == null) {
-						name1 = "";
-					}
 				}
 				jsonEle2 = json.get(i).getAsJsonObject().get("name");
 				if(jsonEle2 == null) {
@@ -55,22 +47,10 @@ public class PetStoreTest extends JerseyTest {
 				}
 				else {
 					name2 = jsonEle2.getAsString();
-					if(name2 == null) {
-						name2 = "";
-					}
 				}
-				//assertTrue(json.get(i-1).getAsJsonObject().get("name").getAsString().compareToIgnoreCase(json.get(i).getAsJsonObject().get("name").getAsString()) <= 0);
-				System.out.println("name1: "+name1);
-				//System.out.println(jsonEle1.toString());
-				System.out.println("name2: " + name2);
-				//System.out.println(jsonEle2.toString());
 				assertTrue(name1.compareToIgnoreCase(name2) <= 0);
 			}
 		}
-		//assertEquals("12", json.get("id"));
-		//assertEquals("Tim", json.get("firstName"));
-		//assertEquals("Tester", json.get("lastName"));
-		//assertEquals("1970-01-16T07:56:49.871+01:00", json.get("birthday"));
 	}
 	
 	@Test
